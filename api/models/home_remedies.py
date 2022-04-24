307cd1bba4044d761f40f5c6e6c1b864ae4139e6
muiz6 <muizhassan83@gmail.com>
2022-04-24 14:07:50 +0500

    moved folder

--- api/models/home_remedies.py
+++
@@ -1,9 +0,0 @@
-from services.mysql import connection_pool
-
-
-def read_home_remedies():
-    cursor = connection_pool.cursor()
-    cursor.execute('SELECT * FROM home_remedies;')
-    result = cursor.fetchall()
-    cursor.close()
-    return [{'id': id, 'name': name, 'image_url': image_url} for id, name, image_url in result]
